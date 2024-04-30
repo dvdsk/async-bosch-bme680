321267e41df1c4e1c178f2a38659698b84febfbe
dvdsk <noreply@davidsk.dev>
2024-04-30 15:00:35 +0200

    re add Debug derive under thiserror feature

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,5 @@
 /// All possible errors
-#[cfg_attr(feature = "thiserror", derive(thiserror::Error))]
+#[cfg_attr(feature = "thiserror", derive(thiserror::Error, Debug))]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub enum BmeError<E> {
     #[cfg_attr(feature = "thiserror", error("Error during I2C write operation: {0}"))]
